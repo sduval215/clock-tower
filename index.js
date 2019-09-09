12346f0d0f0ef7cd45d0023a85473b2e2d327274
Steven Duval Ruilova <sduval215@gmail.com>
2019-09-08 21:48:22 -0700

    add core time checking logic +abstract optimizations

--- index.js
+++ index.js
@@ -8,27 +8,42 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
 });
 
-function getAudioFile(fileName){
-  return Omx(`./audio/${fileName}.mp3`);
+/**
+ * Plays sounds utilizing the node-omxplayer package
+ * @param { String } hour - hour file to be play (1 - 12) 
+ */
+function playSound(hour){
+  const Omx = require('node-omxplayer');
+  Omx(`./audio/${hour}.mp3`);
 }
 
-function checkTime(){
-  // check if time is between 9am and 9pm
-    // if time is between that interval
-      // if time is on the hour
-        // play hour time
+/**
+ * Returns momentJS time based on format argument provided
+ * @param { String } format - momentjs format to return moment value i9n
+ * @returns { String } - returns formatted momentjs time based on format arg
+ */
+function returnFormattedTime(format){
+  const moment = require('moment');
+  return moment().format(format);
 }
-
+ 
 server.listen(PORT, HOSTNAME, () => {
   console.log(`Server running at ${HOSTNAME} on port ${PORT}.`);
 
-  const moment = require('moment');
-  const Omx = require('node-omxplayer');
-
-  setTimeout(() => {
-    console.log('Checking time....');
-    checkTime();
+  setInterval(() => {
+    console.log('checking time...');
+    // get hour (24) as number
+    const currentHour = Number(returnFormattedTime('H'));
+    // if between 9 (9am) and 21 (9pm)
+    if (currentHour >= 9 && currentHour <= 21) {
+      console.log('currently between 9am and 9pm: ', currentHour);
+      const currentMinutesAndSeconds = returnFormattedTime('mm:ss');
+      console.log('current time: ', returnFormattedTime('hh:mm:ss'));
+      if(currentMinutesAndSeconds === '00:00') { // if on the hour (00:00)
+        const hourFile = returnFormattedTime('h');
+        playSound(hourFile);
+      }
+    }  
   }, 1000);
-
   
 });
