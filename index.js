5549d17ad4dba22779167b76c9dad99ebe36c8a7
Steven Duval Ruilova <sduval215@gmail.com>
2019-09-08 20:13:14 -0700

    add initial time checking logic scaffolding

--- index.js
+++ index.js
@@ -6,13 +6,29 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello dev.to!\n');
 });
+
+function getAudioFile(fileName){
+  return Omx(`./audio/${fileName}.mp3`);
+}
+
+function checkTime(){
+  // check if time is between 9am and 9pm
+    // if time is between that interval
+      // if time is on the hour
+        // play hour time
+}
 
 server.listen(PORT, HOSTNAME, () => {
   console.log(`Server running at ${HOSTNAME} on port ${PORT}.`);
 
-  // CODE RUNS HERE
+  const moment = require('moment');
+  const Omx = require('node-omxplayer');
+
+  setTimeout(() => {
+    console.log('Checking time....');
+    checkTime();
+  }, 1000);
 
   
 });
